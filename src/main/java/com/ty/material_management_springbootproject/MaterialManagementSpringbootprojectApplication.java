10ab6dca1f349df7a828582b9bf8784c4d5bc35a
unknown <nayanamsnayana12@gmail.com>
2023-02-22 14:54:40 +0530

    material management

---
+++ src/main/java/com/ty/material_management_springbootproject/MaterialManagementSpringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ty.material_management_springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialManagementSpringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialManagementSpringbootprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/material_management_springbootproject/MaterialManagementSpringbootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.material_management_springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialManagementSpringbootprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
