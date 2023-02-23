69efbfa6e933996cedc44c2067c32dcbade81da2
Nayana-m-s <122531374+Nayana-m-s@users.noreply.github.com>
2023-02-23 14:32:37 +0530

    Delete src/main/java/com/ty/material_management_springbootproject/repository directory

--- src/main/java/com/ty/material_management_springbootproject/repository/Vendor.java
+++
@@ -1,7 +0,0 @@
-package com.ty.material_management_springbootproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Vendor extends JpaRepository<Vendor, Integer>{
-
-}
