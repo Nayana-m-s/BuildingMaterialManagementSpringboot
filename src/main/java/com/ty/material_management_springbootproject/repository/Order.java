aa21d3ac30e60dd31de9449aeaf966c2b72dc93d
sunilkumargithubki <bathalasunilkumar95@gmail.com>
2023-02-23 12:57:56 +0530

    adding repository

---
+++ src/main/java/com/ty/material_management_springbootproject/repository/Order.java
@@ -0,0 +1,7 @@
+package com.ty.material_management_springbootproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Order  extends JpaRepository<Order, Integer>{
+
+}
