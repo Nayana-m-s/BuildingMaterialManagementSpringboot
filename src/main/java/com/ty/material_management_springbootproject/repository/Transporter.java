5d9f394f92b929d84a8051af291ae43a01828f55
sunilkumargithubki <bathalasunilkumar95@gmail.com>
2023-02-23 15:44:16 +0530

    pushing repo

---
+++ src/main/java/com/ty/material_management_springbootproject/repository/Transporter.java
@@ -0,0 +1,7 @@
+package com.ty.material_management_springbootproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Transporter  extends JpaRepository<Transporter, Integer>{
+
+}
---
+++ src/main/java/com/ty/material_management_springbootproject/repository/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author batha
+ *
+ */
+package com.ty.material_management_springbootproject.repository;
